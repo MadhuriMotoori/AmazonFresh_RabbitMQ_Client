5c35bf88eecbe421d9453a6ae6bb9f39b02734af
vaibhavagrawal0289 <vaibhav.agrawal0289@gmail.com>
2016-05-03 22:37:00 -0500

    Changes done for logout

---
+++ public/js/logoutControllerForAdminAndFarmer.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Vaibhav on 5/3/2016.
+ */
+
+routerApp.controller('logoutControllerForAdminAndFarmer', ['$scope','$state','$http',function($scope,$state, $http) {
+
+
+
+$scope.logout=function(){
+    alert("Hi");
+
+    $http({
+        method: "GET",
+        url: "/logout"
+    }).success(function (data) {
+        window.location.assign("/");
+
+    }).error(function (error){
+        console.log(error);
+    });
+
+
+}
+
+}]);
